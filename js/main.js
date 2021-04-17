6a08b647151b72f3749ce93508ecae6b13574b76
nikrinkrda <82508958+nikrinkrda@users.noreply.github.com>
2021-04-17 22:49:38 +0300

    Пофикс меню и шапку, добавили часть контента

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.chennel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 4,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.chennel-button-next',
+    prevEl: '.chennel-button-prev',
+  },
+});
